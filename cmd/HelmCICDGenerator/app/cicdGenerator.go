70c23acd9009d74e20e797f43e6932a65fdb15ae
xgolis <xgolis@stuba.sk>
2023-04-24 16:56:08 +0200

    ach

--- cmd/HelmCICDGenerator/app/cicdGenerator.go
+++ cmd/HelmCICDGenerator/app/cicdGenerator.go
@@ -17,6 +17,12 @@ func createCICDPipelines(app UserApp) error {
 	}
 	cp.Copy(prefix+"/.gitlab-ci.yml", "./"+app.Name)
 
+	err = createGithubPipeline(prefix, app)
+	if err != nil {
+		return fmt.Errorf("error while creating gitlab pipeline: %v", err)
+	}
+	cp.Copy(prefix+"/.github", "./"+app.Name)
+
 	return nil
 }
 
@@ -41,6 +47,34 @@ func createGitlabPipeline(prefix string, app UserApp) error {
 	}
 	output := strings.Join(lines, "\n")
 	err = ioutil.WriteFile("./"+app.Name+"/.gitlab-ci.yml", []byte(output), 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createGithubPipeline(prefix string, app UserApp) error {
+	input, err := ioutil.ReadFile(prefix + "/.github/workflows/deploy.yaml")
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		// if strings.Contains(line, "  IMAGE_NAME: ") {
+		// 	lines[i] = "  IMAGE_NAME: " + app.Image
+		// }
+		if strings.Contains(line, "  NAMESPACE: ") {
+			lines[i] = "  NAMESPACE: " + app.UserName
+		}
+		if strings.Contains(line, "APPNAME: ") {
+			lines[i] = "  APPNAME: " + app.Name
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile("./"+app.Name+"/.github/workflows/deploy.yaml", []byte(output), 0644)
 	if err != nil {
 		return err
 	}
